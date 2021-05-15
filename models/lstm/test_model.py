2b1abff6bff5441c2a21dd55b5f34dcd29a5233a
ikrfun <t.nobuto130625@gmail.com>
2021-05-15 23:04:34 +0900

    文法OK　動作確認未完了

--- models/lstm/test_model.py
+++ models/lstm/test_model.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import torch.optimizer as optim
 
 
-class make_datasets:
+class make_datasets:#データの前処理を行えるクラス
     def __init__(self,csv_file,col_name, type, num_window=0):
         raw_data = pd.read_csv(csv_file)
         self.price = raw_data[col_name]
@@ -17,7 +17,6 @@ class make_datasets:
         self.target_train = []
         self.seq_test = []
         self.target_test = []
-        
         if type == "lstm":
             self.make_data_for_lstm(num_window)
         elif type == "mlp":
@@ -26,26 +25,24 @@ class make_datasets:
             print("type:{}は存在しません".format(type))
         
     def make_data_for_lstm(self, num_window): #LSTMのためのデータセットを作成する
-        num_data = len(self.y)
+        num_data = len(self.price)
         seq_data = []
         target_data = []
         for i in range(num_data - num_window):
-            seq_data.append(y[i:i+num_window])
-            target_data.append(y[i+num_window:1+i+num_window])
-        seq_arr = np.array(seq_data)
-        target_arr = np.array(target_data)
+            seq_data.append(self.price[i:i+num_window])
+            target_data.append(self.price[i+num_window:1+i+num_window])
+        self.seq_arr = np.array(seq_data)
+        self.target_arr = np.array(target_data)
     
     def make_data_for_mlp(self):
         pass#今後実装
     
     def lstm_train_data(self,num_test):
-        return self.seq_train, self.target_train
-    def lstm_test_data(self):
-        return self.seq_test, self.target_test
-    
-        
-    
-    
+        self.seq_train = self.seq_arr[:-num_test]
+        self.seq_target = self.target_arr[:-num_test]
+        self.seq_test = self.seq_arr[-num_test :]
+        self.target_test = self.target_arr[-num_test :]
+        return self.seq_train, self.target_train, self.seq_test, self.target_test
     
 #モデル定義
 class LSTM(nn.Module):
@@ -59,25 +56,6 @@ class LSTM(nn.Module):
         x_last = x[-1]
         x = self.linear(x_last)
         return x
-    
-
-    
-
-data_set = make_datasets("./test_model","opening_price") #インスタンス作成
-y_seq, y_target = data_set.make_data_for_lstm(30)#30日分のデータを１区切りとして学習する
-y_seq_t = y_seq.FloatTensor() #　GPU処理のためにTensor型に変換する
-y_target_t = y_target.FloatTensor()
-y_seq_t = y_seq_t.permute(1,0) #次元の入れ替え
-y_target_t = y_target_t.permute(1,0) 
-y_seq_t = y_seq_t.unsequeeze(dim = -1) #３次元にする
-y_target_t = y_target_t.unsequeeze(dim = -1)
-criterion = nn.MSELoss()
-
-num_hidden = 100 #モデルのLSTMブロックの数を定義
-lr = 0.001 #ラーニングレートを設定
-num_epoch = 100
-model = LSTM(num_hidden)
-optimizer = optim.Adam(model.parameters(),lr = lr)
 
 # 学習
 def learn(seq_data,target_data,num_epoch):
@@ -92,17 +70,33 @@ def learn(seq_data,target_data,num_epoch):
         if epoch % 10 == 0:
             print("epoch{} : loss{}".format(epoch,loss.item()))
         plt.plot(losses)
-        
+
+data_set = make_datasets("./test_model","opening_price","lstm",30) #インスタンス作成 30日分のデータを１区切りとして学習する
+seq_train, target_train, seq_test, target_test = data_set.lstm_train_data(10)
+seq_train_t = seq_train.FloatTensor() #　GPU処理のためにTensor型に変換する
+target_train_t = target_train.FloatTensor()
+seq_train_t = seq_train_t.permute(1,0) #次元の入れ替え
+target_train_t= target_train_t.permute(1,0) 
+seq_train_t = seq_train_t.unsequeeze(dim = -1) #３次元にする
+target_train_t = target_train_t.unsequeeze(dim = -1)
+criterion = nn.MSELoss()
+num_hidden = 100 #モデルのLSTMブロックの数を定義
+lr = 0.001 #ラーニングレートを設定
+num_epoch = 100
+model = LSTM(num_hidden)
+optimizer = optim.Adam(model.parameters(),lr = lr)
+#学習
+learn(seq_train_t,target_train_t,num_epoch)
 #テスト
 #テストデータの整形
-y_seq_test_t = y_seq_test.FloatTensor()
-y_seq_test_t = y_seq_test_t.permute(1,0)
-y_seq_test_t = y_seq_test_t.unsequeeze(dim = -1)
+seq_test_t = seq_test.FloatTensor()
+seq_test_t= seq_test_t.permute(1,0)
+seq_test_t = seq_test_t.unsequeeze(dim = -1)
 #modelにテストデータを流し、予測を行う
-y_pred = model(y_seq_test_t)
+y_pred = model(seq_test_t)
 #予測したデータをPlotしてみる
-x = time_data #使用したデータの時系列情報
-y = trading_price #使用した株価のrawデータ
+x = data_set.date #使用したデータの時系列情報
+y = data_set.price #使用した株価のrawデータ
 plt.plot(x,y)
 plt.plot("日付の範囲(pandas_date_data)",y_pred.detach())
 plt.xlim(["日付の範囲(pandas_date_data"])
